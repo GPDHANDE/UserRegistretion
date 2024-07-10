dbde711bc33a0bcee6cc598eb52b71d072ff1e26
Gaurav <gpdhande12@gmail.com>
2024-07-10 15:29:01 +0530

    checks email pattern

--- src/main/java/Org/UserRegistration/UserRegistration.java
+++ src/main/java/Org/UserRegistration/UserRegistration.java
@@ -11,36 +11,51 @@ public class UserRegistration
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first name: ");
-		String firstName = sc.next();
-		String firstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-		Pattern compiledPattern1 = Pattern.compile(firstNamePattern);
-		Matcher matcher1 = compiledPattern1.matcher(firstName);
+//		System.out.println("Enter first name: ");
+//		String firstName = sc.next();
+//		String firstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+//		Pattern compiledPattern1 = Pattern.compile(firstNamePattern);
+//		Matcher matcher1 = compiledPattern1.matcher(firstName);
+//		
+//		if(matcher1.matches()) 
+//		{
+//			System.out.println("Valid first name.");
+//		}
+//		else
+//		{
+//			System.out.println("Invalid first name.");
+//		}
+//		
+//		System.out.println("Enter last name: ");
+//		String lastName = sc.next();
+//		String lastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+//		Pattern compiledPattern2 = Pattern.compile(lastNamePattern);
+//		Matcher matcher2 = compiledPattern2.matcher(lastName);
+//		
+//		if(matcher2.matches()) 
+//		{
+//			System.out.println("Valid last name.");
+//		}
+//		else
+//		{
+//			System.out.println("Invalid last name.");
+//		}
 		
-		if(matcher1.matches()) 
+		System.out.println("Enter the email: ");
+		String email = sc.next();
+		String emailPattern = "^[a-zA-Z0-9._%+-]+\\.[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}(\\.[a-zA-Z]{2,6})?$";
+		
+		Pattern compiledPattern3 = Pattern.compile(emailPattern);
+		Matcher matcher3 = compiledPattern3.matcher(email);
+		
+		if(matcher3.matches())
 		{
-			System.out.println("Valid first name.");
+			System.out.println("Valid Email.");
 		}
 		else
 		{
-			System.out.println("Invalid first name.");
+			System.out.println("Invalid Email.");
 		}
-		
-		System.out.println("Enter last name: ");
-		String lastName = sc.next();
-		String lastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-		Pattern compiledPattern2 = Pattern.compile(lastNamePattern);
-		Matcher matcher2 = compiledPattern2.matcher(lastName);
-		
-		if(matcher2.matches()) 
-		{
-			System.out.println("Valid last name.");
-		}
-		else
-		{
-			System.out.println("Invalid last name.");
-		}
-		
 	}
 
 }
